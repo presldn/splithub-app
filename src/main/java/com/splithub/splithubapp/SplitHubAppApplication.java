7e3199675dede0c3f100af20f7676126a6921ad9
presldn <ebonsu1996@yahoo.com>
2022-07-11 01:11:48 +0100

    Initial commit

---
+++ src/main/java/com/splithub/splithubapp/SplitHubAppApplication.java
@@ -0,0 +1,13 @@
+package com.splithub.splithubapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitHubAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SplitHubAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/splithub/splithubapp/SplitHubAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.splithub.splithubapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitHubAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
